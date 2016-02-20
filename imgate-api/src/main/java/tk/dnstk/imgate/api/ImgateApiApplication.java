d090957023a10aee4901d927035322ff2df780db
Zhou Feng <feng.zh@gmail.com>
2016-02-20 14:33:38 +0800

    Update swagger doc by expose imgate api only.

--- imgate-api/src/main/java/tk/dnstk/imgate/api/ImgateApiApplication.java
+++ imgate-api/src/main/java/tk/dnstk/imgate/api/ImgateApiApplication.java
@@ -2,7 +2,12 @@ package tk.dnstk.imgate.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -10,6 +15,15 @@ public class ImgateApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ImgateApiApplication.class, args);
+    }
+
+    @Bean
+    public Docket imgateApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("imgate-api")
+                .select()
+                .apis(basePackage(getClass().getPackage().getName()))
+                .build();
     }
 
 }
